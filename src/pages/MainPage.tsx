82354a5297fe8e77cb2f723cf2b67c8eecc052b6
weronikaciezak <weronikaciezak@wp.pl>
2026-03-07 00:56:52 +0100

    simplify Name component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -7,38 +7,31 @@ export const MainPage = () => {
 
     return (
         <Layout title="Weronika Ciężak | Portfolio">
+
             <Title>SOFTWARE DEVELOPER</Title>
-
-            <div style={{display: 'flex', gap: '10px', fontSize: '2rem', padding: '0 0 0.6rem 0'}}>
-                <Name>hi I'm Weronika Ciężak</Name>
-                <div>🖐</div>
-            </div>
-
+            <Name>hi I'm Weronika Ciężak 🖐</Name>
             <div>I am a computer science student from Poland 🇵🇱</div>
             <div>I create full-stack applications. </div>
 
             <H2>Tools I worked with</H2>
-
             <SkillsSection />
+
         </Layout>
     )
 }
 
 const Title = styled.div`
-    background: ${ColorPalette.header.hex};
-    background-clip: text;
-    color: transparent;
+    color: ${ColorPalette.header.hex};
     font-family: "Unbounded", sans-serif;
     font-size: 1.2rem;
     padding: 5.5rem 0 0.2rem 0;
 `;
 
 const Name = styled.div`
-    background: ${ColorPalette.text.hex};
-    background-clip: text;
-    color: transparent;
+    color: ${ColorPalette.text.hex};
     font-family: "Unbounded", sans-serif;
     font-size: 2.3rem;
+    padding: 0 0 0.8rem 0;
 `;
 
 const H2 = styled.div`
