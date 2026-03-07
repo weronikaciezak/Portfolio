02fba02aac2a466503030479750b685fd7a0a158
weronikaciezak <weronikaciezak@wp.pl>
2026-03-07 15:00:12 +0100

    refactor Navbar component; enhance layout with responsive design and adjust container styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,26 +15,50 @@ export const Navbar = () => {
                 <SmallTitle onClick={redirectResume}>Resume</SmallTitle>
                 <SmallTitle onClick={redirectContact}>Contact</SmallTitle>
             </Container>
-            <Container gap={'0.5rem'}>
+            <Container2 gap={'0.5rem'}>
                 <Dot></Dot>
                 <Font>Open to work</Font>
                 <Switch/>
-            </Container>
+            </Container2>
         </Wrapper>
     )
 }
 
 const Wrapper = styled.div`
+    width: 100%;
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 3rem 1rem;
+    
+    @media (max-width: 700px) {
+        align-items: flex-start;
+        padding: 2rem 1rem;
+    }
 `;
 
-const Container = styled.div<{gap?: string}>`
+const Container = styled.div`
     display: flex;
     align-items: center;
+    gap: 3.5rem;
+    flex-wrap: wrap;
+    width: 100%;
+    @media (max-width: 700px) {
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 3px;
+    }
+`;
+const Container2 = styled.div<{gap?: string}>`
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
     gap: ${props => props.gap || '3.5rem'};
+    padding: 0 1.5rem 0 0;
+    @media (max-width: 700px) {
+        gap: 5px;
+    }
 `;
 
 const SmallTitle = styled.div`
