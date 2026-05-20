607353c471e7813f3b4f7962acd80b0058c0322f
weronika.ciezak <weronikaciezak@wp.pl>
2026-05-20 21:02:30 +0200

    refactor Footer component; enhance layout, add social media links, and improve styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,20 +4,64 @@ export const Footer = () => {
     return (
         <>
             <Container></Container>
-            <Text>© 2026 Weronika Ciężak</Text>
+            <Container3>
+                <Text>© 2026 Weronika Ciężak</Text>
+                <Container2>
+                    <Icon
+                        title="Open GitHub"
+                        as="a"
+                        href={`https://github.com/weronikaciezak`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <i className="fa-brands fa-github fa-s"></i>
+                    </Icon>
+
+                    <Icon
+                        title="Open LinkedIn"
+                        as="a"
+                        href={`https://www.linkedin.com/in/weronika-ci%C4%99%C5%BCak-1554b8359/`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <i className="fa-brands fa-linkedin fa-s"></i>
+                    </Icon>
+
+                    <Icon title="Email me"
+                          as="a"
+                          href={`mailto:weronikaciezak@wp.pl`}>
+                        <i className={`fa-solid fa-s fa-envelope`}></i>
+                    </Icon>
+                </Container2>
+            </Container3>
         </>
     )
 }
 
 const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    font-size: 0.8rem;
-    padding: 10rem 0.5rem 0.5rem 0.5rem;
+    padding-top: var(--margin-xxxl);
     border-bottom: 1px solid rgba(255, 255, 255, 0.1);
 `;
 
-const Text = styled.p`
-    padding: 0.1rem 0 1rem 0;
+const Container2 = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 0.5rem;
+`;
+
+const Container3 = styled.div`
+    display: flex;
+    padding-top: 0.6rem;
+    padding-bottom: 1.5rem;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+const Text = styled.div`
     font-size: 0.7rem;
+`;
+
+const Icon = styled.div`
+    cursor: pointer;
+    color: var(--text-color);
 `;
