7c205d306990866d1c72c1007728d67ea0b9f0a3
weronikaciezak <weronikaciezak@wp.pl>
2026-05-21 19:03:22 +0200

    update background gradients and adjust color opacity for improved visual appeal

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -5,7 +5,7 @@ export const Background = createGlobalStyle`
         background-color: var(--background);
         background-image:
                 radial-gradient(rgba(255, 255, 255, var(--opacity)) 1px, transparent 1px),
-                linear-gradient(to bottom, var(--bg-color) 0%, var(--bg3-color) 20%, var(--bg3-color) 80%, var(--bg2-color) 100%);
+                linear-gradient(to bottom, var(--bg2-color) 0%, var(--bg3-color) 20%, var(--bg3-color) 90%, var(--bg-color) 100%);
                 //linear-gradient(to bottom, var(--bg-color) 0%, var(--background) 100%);
         background-size: 20px 20px, 100% 100%;
         background-position: 0 0, 0 0;
